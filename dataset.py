70d649310535ad6a1cb366dc5679667a0e569ae2
phu doan ngoc <doanphucck1801@gmail.com>
2021-06-18 23:53:53 +0700

    Add RandAugment

--- dataset.py
+++ dataset.py
@@ -1,27 +1,34 @@
 from torch.utils.data import Dataset
-from torchvision.transforms import Compose, ToTensor, Normalize, Resize
 import json
-import cv2
+from PIL import Image
 import torch
 
 
+from utils.augment_utils import RandAugment, Resize, ToTensor, Normalize
+
+
 class DogBreedDataset(Dataset):
-    def __init__(self, annotation_path):
+    def __init__(self, annotation_path, width=256, height=256, num_aug=10, magnitude=15):
         super(DogBreedDataset, self).__init__()
         with open(annotation_path, 'r') as f:
             self.annotation = list(json.load(f).items())
+        self.width = width
+        self.height = height
+        self.num_aug = num_aug
+        self.magnitude = magnitude
 
-        self.transforms = Compose([
-            ToTensor(),
-            Resize((256, 256)),
-            Normalize([0.5, 0.5, 0.5],
-                      [0.5, 0.5, 0.5])
-        ])
+    def _transform(self, image):
+        image = RandAugment(self.num_aug, self.magnitude)(image)
+        image = Resize(self.width, self.height)(image)
+        image = ToTensor()(image)
+        image = Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])(image)
+
+        return image
 
     def __getitem__(self, item):
         image_path = 'data/train/' + self.annotation[item][0]
-        image = cv2.imread(image_path)
-        image = self.transforms(image)
+        image = Image.open(image_path)
+        image = self._transform(image)
 
         label = self.annotation[item][1]
         label = torch.tensor(label, dtype=torch.long)
@@ -35,5 +42,5 @@ class DogBreedDataset(Dataset):
 
 if __name__ == '__main__':
     dataset = DogBreedDataset(annotation_path='data/train.json')
-    print(dataset[0][1])
+    print(dataset[0][0])
     pass
---
+++ utils/augment_utils.py
@@ -0,0 +1,185 @@
+import PIL
+import torch
+from PIL import Image, ImageOps, ImageEnhance
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+
+def shear_x(image, value):  # [-0.3: 0.3]
+    assert -0.3 <= value <= 0.3
+    if random.random() > 0.5:
+        value = -value
+
+    return image.transform(image.size, PIL.Image.AFFINE, (1, value, 0, 0, 1, 0))
+
+
+def shear_y(image, value):  # [-0.3: 0.3]
+    assert -0.3 <= value <= 0.3
+    if random.random() > 0.5:
+        value = -value
+
+    return image.transform(image.size, PIL.Image.AFFINE, (1, 0, 0, value, 1, 0))
+
+
+def translate_x(image, value):  # [-0.45, 0.45]
+    assert -0.45 <= value <= 0.45
+    if random.random() > 0.5:
+        value = -value
+
+    value *= image.size[0]
+
+    return image.transform(image.size, PIL.Image.AFFINE, (1, 0, value, 0, 1, 0))
+
+
+def translate_y(image, value):  # [-0.45, 0.45]
+    assert -0.45 <= value <= 0.45
+    if random.random() > 0.5:
+        value = -value
+
+    value *= image.size[1]
+
+    return image.transform(image.size, PIL.Image.AFFINE, (1, 0, 0, 0, 1, value))
+
+
+def rotate(image, value):  # [-30, 30]
+    assert -30 <= value <= 30
+    if random.random() > 0.5:
+        value = -value
+
+    return image.rotate(value)
+
+
+def auto_contrast(image, _):
+
+    return ImageOps.autocontrast(image)
+
+
+def invert(image, _):
+
+    return ImageOps.invert(image)
+
+
+def equalize(image, _):
+
+    return ImageOps.equalize(image)
+
+
+def flip(image, _):
+
+    return ImageOps.mirror(image)
+
+
+def solarize(image, _):
+
+    return ImageOps.solarize(image)
+
+
+def posterize(image, value):  # [1, 8]
+    assert 1 <= value <= 8
+    value = int(value)
+    value = max(1, value)
+
+    return ImageOps.posterize(image, value)
+
+
+def contrast(image, value):  # [0.1: 1.9]
+    assert 0.1 <= value <= 1.9
+
+    return ImageEnhance.Contrast(image).enhance(value)
+
+
+def color(image, value):  # [0.1: 1.9]
+    assert 0.1 <= value <= 1.9
+
+    return ImageEnhance.Color(image).enhance(value)
+
+
+def brightness(image, value):  # [0.1: 1.9]
+    assert 0.1 <= value <= 1.9
+
+    return ImageEnhance.Brightness(image).enhance(value)
+
+
+def sharpness(image, value):  # [0.1: 1.9]
+    assert 0.1 <= value <= 1.9
+
+    return ImageEnhance.Sharpness(image).enhance(value)
+
+
+def identity(image, _):
+
+    return image
+
+
+def augment_list():
+    aug_list = [
+        (auto_contrast, 0, 1),
+        (equalize, 0, 1),
+        (invert, 0, 1),
+        (rotate, -30, 30),
+        (posterize, 1, 8),
+        (solarize, 0, 256),
+        (color, 0.1, 1.9),
+        (contrast, 0.1, 1.9),
+        (brightness, 0.1, 1.9),
+        (sharpness, 0.1, 1.9),
+        (shear_x, -0.3, 0.3),
+        (shear_y, -0.3, 0.3),
+        (translate_x, -0.45, 0.45),
+        (translate_y, -0.45, 0.45)
+    ]
+
+    return aug_list
+
+
+class RandAugment(object):
+    def __init__(self, n, m):
+        self.num_select = n
+        self.magnitude = m
+        self.augment_list = augment_list()
+
+    def __call__(self, image):
+        ops = random.choices(self.augment_list, k=self.num_select)
+        for op, min_value, max_value in ops:
+            val = (float(self.magnitude) / 30) * (max_value - min_value) + min_value
+            image = op(image, val)
+
+        return image
+
+
+class Resize(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __call__(self, image):
+
+        return image.resize((self.width, self.height))
+
+
+class ToTensor(object):
+    def __call__(self, image):
+        image = np.array(image).astype(float)
+        image = image / 255
+        image = torch.from_numpy(image)
+        image = image.permute(2, 0, 1)
+
+        return image
+
+
+class Normalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, image):
+        image[0] = (image[0] - self.mean[0]) / self.std[0]
+        image[1] = (image[1] - self.mean[1]) / self.std[1]
+        image[2] = (image[2] - self.mean[2]) / self.std[2]
+
+        return image
+
+
+if __name__ == '__main__':
+    pass
